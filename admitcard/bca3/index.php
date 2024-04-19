d857a1212824035aeafd4771abde636986df7463
Sabkacode <134566303+upraity@users.noreply.github.com>
2024-04-19 21:48:31 +0530

    Home Page

---
+++ admitcard/bca3/index.php
@@ -0,0 +1,11 @@
+<?php
+error_reporting(0);
+    include("connection.php");
+?>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<form action="index1.php" method="POST">
+<input type="text" placeholder="Enter your rollno" name="rollno" required>
+<input type="Submit">
+</form>
