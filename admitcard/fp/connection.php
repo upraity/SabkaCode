fc6d6c2a85d263a4417a2500a22358ead32c61d6
Sabkacode <134566303+upraity@users.noreply.github.com>
2024-04-22 22:41:40 +0530

    Connection Page

---
+++ admitcard/fp/connection.php
@@ -0,0 +1,16 @@
+<?php
+$dbhost = "";
+$dbuser = "";
+$dbpass = "";
+$dbname = ""; 
+
+$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+
+if (!$conn){
+    //echo "success";
+//}
+//else{
+    die("Error". mysqli_connect_error());
+}
+
+?>
