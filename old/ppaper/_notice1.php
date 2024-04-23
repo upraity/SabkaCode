801476c2e7b8df3ef34f9458c8440ca7716d67ee
Sabkacode <134566303+upraity@users.noreply.github.com>
2024-04-23 22:20:59 +0530

    Notice

---
+++ old/ppaper/_notice1.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Login Page</title>
+        <link rel="stylesheet" href="sagar1.css">
+        <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'> 
+        <style>
+            .notice{
+                border:2px solid #fff;
+                padding:20px;
+                margin:10px;
+            }
+
+            .notice p{
+                text-align:left;
+                width:100%;
+                height:100%;
+            }
+        </style>
+    </head>
+    <body>
+        <header class="header">
+            <nav class="navbar">
+                <a href="https://dj.000.pe/old/l.php">Home</a>
+                <a href="_notice1.php">Notice</a>
+                <a href="_contact1.php">Contact</a>
+            </nav>
+
+            <form action="#" class="search-bar">
+                <input type="text" placeholder="Search....">
+                <button type="submit"><i class='bx bx-search'></i></button>
+            </form>
+        </header>
+        <div class="background"></div>
+        <div class="contanier">
+            <div class="content">
+                <h1 class="logo"><i class='bx bxl-flutter'></i>Sabka Code</h1>
+                <div class="text-sci">
+                    <h2>Welcome!<br><span>To Our New Website.</span></h2>
+                    <p>Learn and Study the Code</p>
+                    
+                    <div class="social-icons">
+                         <!--<a href="#"><i class='bx bxl-linkedin'></i></a>-->
+                        <a href="https://www.facebook.com/profile.php?id=100094393995814&mibextid=ZbWKwL"><i class='bx bxl-facebook'></i></a>
+                        <a href="https://www.instagram.com/sabkacode/"><i class='bx bxl-instagram'></i></a>
+                        <a href="https://twitter.com/sabkacode"><i class='bx bxl-twitter'></i></a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="logreg-box">
+                <div class="form-box login">
+                    <form action="#">
+                        <div class="notice">
+                            <pre><p><b> 1.</b> All subject <b>(C, C++, DSA, NM, Maths, DE, HTML)</b><br>previous paper are also here.<br></p></pre>
+                            <pre><p><b> 2.</b> Syllabus and <b></b>notes of all the subjects.<br></p></pre>
+                            <pre><p><b> 3.</b> <b>Practicle Question and Assignment</b> are available.<br></p></pre>
+                            <pre><p><b> 4.</b> You contact us to our team for any query. <br></p></pre>
+                            <pre><p><b> 5.</b> And you see and <b>download books</b> in any subject and <br>different writter.<br></p></pre>             
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <script src="sagar.js"></script>
+    </body>
+</html>
