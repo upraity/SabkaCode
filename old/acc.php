05a35f8293859ebc03cbe501dcccc3eda1e24717
Sabkacode <134566303+upraity@users.noreply.github.com>
2024-04-24 22:55:34 +0530

    Account

---
+++ old/acc.php
@@ -0,0 +1,45 @@
+<html>
+    <head>
+      <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+        <style>
+            abbr{
+                text-decoration:none;
+            }
+            .account{
+                text-align:right;
+                font-size:21px;
+                text-decoration:none;
+            }
+
+            .account a{
+                text-decoration:none;
+                color:black;
+            }
+
+            .account a::visited{
+                text-decoration:none;
+                color:black;
+            }
+
+            .acc{
+                font-size:25px;
+            }
+
+            #acc{
+                font-size:27px;
+            }
+        </style>
+    </head>
+    <body>
+                 
+            <abbr title="My Account">
+                <div class="account">
+                    <a href="https://dj.000.pe/old/account.php">
+                        <span class="icon"><i class='bx bxs-user-account' id="acc"></i></span>
+                        <label class="acc">Profile</label>
+                    </a>
+                </div>
+            </abbr>
+     
+    </body>
+</html>
