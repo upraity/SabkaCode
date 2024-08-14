f3a76913bb3b04f2596b94d0301028efab49441b
Dj <134566303+upraity@users.noreply.github.com>
2024-08-14 15:40:09 +0530

    Create handle_add.php

---
+++ TalkSta/profiles/handle_add.php
@@ -0,0 +1,22 @@
+<?php
+    include 'dbcon.php';
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        $title = $_POST['title'];
+        $desc = $_POST['desc'];
+    
+        $sql = "INSERT INTO `categories` (`category_name`, `category_desc`, `datetime`) VALUES ('$title', '$desc', current_timestamp()) ";
+        $result = mysqli_query($conn, $sql);
+        if($result){
+            echo "<script>alert('Insert successfully');</script>";
+            header("refresh:0; url= https://dj.000.pe/talksta");
+        }
+        else{
+            echo "<script>alert('Not insert, \nsome problem...');</script>";
+            header("refresh:0; url= https://dj.000.pe/talksta");
+        }   
+    }
+    else{
+        echo "<script>alert('Accha, maje le raha he!!');</script>";
+        header("refresh:0; url= https://dj.000.pe/talksta");
+    }
+?>
