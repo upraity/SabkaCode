5978fbea26051cd0385a3d22bb61cedf6baf3411
Dj <134566303+upraity@users.noreply.github.com>
2024-09-23 22:18:20 +0530

    Create C.php

---
+++ old/C.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Project of C</title>
+        <link rel="stylesheet" href="styleC.css"> 
+    </head>
+    <body>
+         <center><h2>!!Radhe Radhe!!</h2></center>
+     <div class="home"> 
+         <?php include('nav.php'); ?>
+         <br><br>
+          <div class="dj">     
+  <a href="https://www.highwaycpmrevenue.com/zbh166e5?key=b1af681cb44d76527048c34d1baa443b"><h1>Don't Click</h1></a>
+  </div><br>
+         <div class="imp">
+             <h2>Download the practical file of C language.</h2>
+             <p>This is a practical file inside these you see the questions i mean programs and solution </p>
+             <center><a href="complete file.pdf" download>Download here</a></center><br><br>
+         </div>
+         <br>
+           <?php include('footer.php');?> 
+     </div>
+
+    </body>
+</html>         
